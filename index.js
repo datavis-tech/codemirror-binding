71114f591df207b4230986aba72873b55d0389de
curran <curran.kelleher@gmail.com>
2017-07-10 22:07:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// This file began as a copy of https://github.com/share/sharedb-string-binding/blob/master/index.js
+var TextDiffBinding = require('./textDiffCodeMirrorBinding');
+
+function CodeMirrorBinding(element, doc, path) {
+  TextDiffBinding.call(this, element);
+  this.doc = doc;
+  this.path = path || [];
+  this._opListener = null;
+  this._inputListener = null;
+}
+CodeMirrorBinding.prototype = Object.create(TextDiffBinding.prototype);
+CodeMirrorBinding.prototype.constructor = CodeMirrorBinding;
+
+CodeMirrorBinding.prototype.setup = function() {
+  this.update();
+  this.attachDoc();
+  this.attachElement();
+};
+
+CodeMirrorBinding.prototype.destroy = function() {
+  this.detachElement();
+  this.detachDoc();
+};
+
+CodeMirrorBinding.prototype.attachElement = function() {
+  var binding = this;
+  this._inputListener = function() {
+    binding.onInput();
+  };
+  this.element.addEventListener('input', this._inputListener, false);
+};
+
+CodeMirrorBinding.prototype.detachElement = function() {
+  this.element.removeEventListener('input', this._inputListener, false);
+};
+
+CodeMirrorBinding.prototype.attachDoc = function() {
+  var binding = this;
+  this._opListener = function(op, source) {
+    binding._onOp(op, source);
+  };
+  this.doc.on('op', this._opListener);
+};
+
+CodeMirrorBinding.prototype.detachDoc = function() {
+  this.doc.removeListener('op', this._opListener);
+};
+
+CodeMirrorBinding.prototype._onOp = function(op, source) {
+  if (source === this) return;
+  if (op.length === 0) return;
+  if (op.length > 1) {
+    throw new Error('Op with multiple components emitted');
+  }
+  var component = op[0];
+  if (isSubpath(this.path, component.p)) {
+    this._parseInsertOp(component);
+    this._parseRemoveOp(component);
+  } else if (isSubpath(component.p, this.path)) {
+    this._parseParentOp();
+  }
+};
+
+CodeMirrorBinding.prototype._parseInsertOp = function(component) {
+  if (!component.si) return;
+  var index = component.p[component.p.length - 1];
+  var length = component.si.length;
+  this.onInsert(index, length);
+};
+
+CodeMirrorBinding.prototype._parseRemoveOp = function(component) {
+  if (!component.sd) return;
+  var index = component.p[component.p.length - 1];
+  var length = component.sd.length;
+  this.onRemove(index, length);
+};
+
+CodeMirrorBinding.prototype._parseParentOp = function() {
+  this.update();
+};
+
+CodeMirrorBinding.prototype._get = function() {
+  var value = this.doc.data;
+  for (var i = 0; i < this.path.length; i++) {
+    var segment = this.path[i];
+    value = value[segment];
+  }
+  return value;
+};
+
+CodeMirrorBinding.prototype._insert = function(index, text) {
+  var path = this.path.concat(index);
+  var op = {p: path, si: text};
+  this.doc.submitOp(op, {source: this});
+};
+
+CodeMirrorBinding.prototype._remove = function(index, text) {
+  var path = this.path.concat(index);
+  var op = {p: path, sd: text};
+  this.doc.submitOp(op, {source: this});
+};
+
+function isSubpath(path, testPath) {
+  for (var i = 0; i < path.length; i++) {
+    if (testPath[i] !== path[i]) return false;
+  }
+  return true;
+}
+
+module.exports = CodeMirrorBinding;
---
+++ textDiffCodeMirrorBinding.js
@@ -0,0 +1,101 @@
+// This file began as a copy of https://github.com/share/text-diff-binding/blob/master/index.js
+module.exports = TextDiffBinding;
+
+function TextDiffBinding(element) {
+  this.element = element;
+}
+
+TextDiffBinding.prototype._get =
+TextDiffBinding.prototype._insert =
+TextDiffBinding.prototype._remove = function() {
+  throw new Error('`_get()`, `_insert(index, length)`, and `_remove(index, length)` prototype methods must be defined.');
+};
+
+TextDiffBinding.prototype._getElementValue = function() {
+  var value = this.element.value;
+  // IE and Opera replace \n with \r\n. Always store strings as \n
+  return value.replace(/\r\n/g, '\n');
+};
+
+TextDiffBinding.prototype._getInputEnd = function(previous, value) {
+  if (this.element !== document.activeElement) return null;
+  var end = value.length - this.element.selectionStart;
+  if (end === 0) return end;
+  if (previous.slice(previous.length - end) !== value.slice(value.length - end)) return null;
+  return end;
+};
+
+TextDiffBinding.prototype.onInput = function() {
+  var previous = this._get();
+  var value = this._getElementValue();
+  if (previous === value) return;
+
+  var start = 0;
+  // Attempt to use the DOM cursor position to find the end
+  var end = this._getInputEnd(previous, value);
+  if (end === null) {
+    // If we failed to find the end based on the cursor, do a diff. When
+    // ambiguous, prefer to locate ops at the end of the string, since users
+    // more frequently add or remove from the end of a text input
+    while (previous.charAt(start) === value.charAt(start)) {
+      start++;
+    }
+    end = 0;
+    while (
+      previous.charAt(previous.length - 1 - end) === value.charAt(value.length - 1 - end) &&
+      end + start < previous.length &&
+      end + start < value.length
+    ) {
+      end++;
+    }
+  } else {
+    while (
+      previous.charAt(start) === value.charAt(start) &&
+      start + end < previous.length &&
+      start + end < value.length
+    ) {
+      start++;
+    }
+  }
+
+  if (previous.length !== start + end) {
+    var removed = previous.slice(start, previous.length - end);
+    this._remove(start, removed);
+  }
+  if (value.length !== start + end) {
+    var inserted = value.slice(start, value.length - end);
+    this._insert(start, inserted);
+  }
+};
+
+TextDiffBinding.prototype.onInsert = function(index, length) {
+  this._transformSelectionAndUpdate(index, length, insertCursorTransform);
+};
+function insertCursorTransform(index, length, cursor) {
+  return (index < cursor) ? cursor + length : cursor;
+}
+
+TextDiffBinding.prototype.onRemove = function(index, length) {
+  this._transformSelectionAndUpdate(index, length, removeCursorTransform);
+};
+function removeCursorTransform(index, length, cursor) {
+  return (index < cursor) ? cursor - Math.min(length, cursor - index) : cursor;
+}
+
+TextDiffBinding.prototype._transformSelectionAndUpdate = function(index, length, transformCursor) {
+  if (document.activeElement === this.element) {
+    var selectionStart = transformCursor(index, length, this.element.selectionStart);
+    var selectionEnd = transformCursor(index, length, this.element.selectionEnd);
+    var selectionDirection = this.element.selectionDirection;
+    this.update();
+    this.element.setSelectionRange(selectionStart, selectionEnd, selectionDirection);
+  } else {
+    this.update();
+  }
+};
+
+TextDiffBinding.prototype.update = function() {
+  var value = this._get();
+  if (this._getElementValue() === value) return;
+  this.element.value = value;
+};
